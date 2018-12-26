44d3d115abaf47f897886cbe9c1472bf542d8706
mitulp534 <mitulp534@gmail.com>
2018-12-27 01:44:53 +0530

    deleted unused files and app works fine still need to add info about other views

--- app/src/main/java/com/example/hp/com/kidslearn/AbcdActivity.java
+++ app/src/main/java/com/example/hp/com/kidslearn/AbcdActivity.java
@@ -1,15 +1,9 @@
 package com.example.hp.com.kidslearn;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -18,7 +12,7 @@ public class AbcdActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.abcd_activity);
+        setContentView(R.layout.words_list);
 
         //create an arraylist of alphabets
         ArrayList<abcd_main> alphabet = new ArrayList<abcd_main>();
--- app/src/main/java/com/example/hp/com/kidslearn/AnimalsActivity.java
+++ app/src/main/java/com/example/hp/com/kidslearn/AnimalsActivity.java
@@ -3,13 +3,38 @@ package com.example.hp.com.kidslearn;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AnimalsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.animals_activity);
+        setContentView(R.layout.words_list);
+
+        //create an arraylist of alphabets
+        ArrayList<abcd_main> alphabet = new ArrayList<abcd_main>();
+        alphabet.add(new abcd_main("Cow","Cow gives milk"));
+        alphabet.add(new abcd_main("Dog","Dogs are honest"));
+        alphabet.add(new abcd_main("Cat","Cat do meow meow"));
+        alphabet.add(new abcd_main("Tiger","Nation animal of india"));
+        alphabet.add(new abcd_main("Lion","King of forest"));
+        alphabet.add(new abcd_main("Leopard","Fastest running animal"));
+        alphabet.add(new abcd_main("Elephant","Have long nose"));
+
+        //LinearLayout abcdRootView =(LinearLayout)findViewById(R.id.abcdRootView);
+        abcdAdapter adapter = new abcdAdapter(this,alphabet);
+
+        ListView abcdlistView = (ListView) findViewById(R.id.abcdRootView);
+
+        abcdlistView.setAdapter(adapter);
+
+
+
 
     }
+
+
 }
